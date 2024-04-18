85f753218ee85f76ad67567a616c0dc0496f4220
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-18 21:02:42 +0200

    added default card messages

--- src/CustomKanban.jsx
+++ src/CustomKanban.jsx
@@ -302,30 +302,24 @@
 
     const DEFAULT_CARDS = [
     // BACKLOG
-    { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
-    { title: "SOX compliance checklist", id: "2", column: "backlog" },
-    { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
-    { title: "Document Notifications service", id: "4", column: "backlog" },
+    { title: "This is a Kanban board", id: "1", column: "backlog" },
+    { title: "Play around by adding tasks", id: "2", column: "backlog" },
+    { title: "You can move tasks by dragging and dropping", id: "3", column: "backlog" },
     // TODO
-    {
-        title: "Research DB options for new microservice",
-        id: "5",
-        column: "todo",
-    },
-    { title: "Postmortem for outage", id: "6", column: "todo" },
-    { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+    { title: "Move tasks to trash if you want to delete them", id: "5", column: "todo" },
+    { title: "Your productivity roadmap", id: "6", column: "todo" },
 
     // DOING
     {
-        title: "Refactor context providers to use Zustand",
-        id: "8",
+        title: "Be as productive as possible",
+        id: "7",
         column: "doing",
     },
-    { title: "Add logging to daily CRON", id: "9", column: "doing" },
+    { title: "Log all your daily tasks", id: "8", column: "doing" },
     // DONE
     {
-        title: "Set up DD dashboards for Lambda listener",
-        id: "10",
+        title: "Hope you enjoy!!",
+        id: "9",
         column: "done",
     },
     ];
